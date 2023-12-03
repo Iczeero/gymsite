6eea92a4343700f7bb1f70014a59c84693e04737
Iczeero <57349536+Iczeero@users.noreply.github.com>
2023-12-03 15:12:11 +0300

    modified:   Pipfile 	modified:   Pipfile.lock 	modified:   backend/gym/exercises/models.py

--- backend/gym/exercises/models.py
+++ backend/gym/exercises/models.py
@@ -7,9 +7,32 @@ class Users(models.Model):
     email = models.CharField(max_length=255)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
 class Exercises(models.Model):
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
 
 class Programs(models.Model):
     name = models.CharField(max_length=255)
